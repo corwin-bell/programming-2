995202752d0957687ab43937373d1d5005369042
corwin-bell <corwin.m.bell@gmail.com>
2024-07-17 17:29:58 -0600

    Add tests for calculation methods

--- module1_critthink_bankaccount/src/BankAccount.java
+++ module1_critthink_bankaccount/src/BankAccount.java
@@ -14,9 +14,27 @@ public class BankAccount {
         this.balance = 0;
     }
     // deposit() - will accept a single value double parameter; the parameter value is added to the existing balance
+    public void deposit(double amountIn) {
+        balance += amountIn;
+    }
     // withdrawal() - accepts a single value double dollar amount; the parameter value is subtracted from the existing balance
+    public void withdrawal(double amountOut) {
+        balance -= amountOut;
+    }
     // Setters and getters for firstName, lastName, and accountID
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
     // getBalance() getter to return the balance
+    public double getBalance() {
+        return balance;
+    }
     // accountSummary() - prints all account information
     public void accountSummary() {
         String summaryString = String.format("First Name: %s \nLast Name: %s \nAccount ID: %d \nBalance: $%.2f",
--- module1_critthink_bankaccount/src/BankTest.java
+++ module1_critthink_bankaccount/src/BankTest.java
@@ -2,5 +2,11 @@ public class BankTest {
     public static void main(String[] args) throws Exception {
         BankAccount myAccount = new BankAccount("Corwin", "Bell", 1);
         myAccount.accountSummary();
+        myAccount.deposit(10);
+        System.out.printf("expecting balance to be $10.00, got $%.2f\n", myAccount.getBalance());
+        myAccount.withdrawal(10);
+        System.out.printf("expecting balance to be $0, got $%.2f\n", myAccount.getBalance());
+        // would be cool to use assertions but it's not working :(
+        // assert myAccount.getBalance() == 10 : "deposit was " + myAccount.getBalance() + ". Should be 10";
     }
 }
