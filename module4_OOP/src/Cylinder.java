f6d88e23544aea45c3283a3e78faf6f9e844deb2
corwin-bell <corwin.m.bell@gmail.com>
2024-08-07 11:01:04 -0600

    Add Cylinder class members

--- module4_OOP/src/Cylinder.java
+++ module4_OOP/src/Cylinder.java
@@ -1,3 +1,42 @@
-public class Cylinder {
+public class Cylinder  extends Sphere{
+    private double radius;
+    private double height;
 
+    // constructors
+    Cylinder() {
+        this.radius = 0;
+        this.height = 0;
+    }
+    
+    Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    // methods
+    public void setRadius(double radius) { this.radius = radius; }
+    public double getRadius() { return radius; }
+    public void setHeight(double height) { this.height = height; }
+    public double getHeight() { return height; }
+    
+    @Override
+    double surfaceArea() {
+        double circles = 2 * Math.PI * Math.pow(radius, 2);
+        double rectangle = 2 * Math.PI * radius * height;
+        return circles + rectangle;
+    }
+
+    @Override
+    double volume() {
+        return height * Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        String infoString = String.format("Cylinder stats\nRadius: %.2f\nSurface Area: %.2f\nVolume: %.2f\n",
+        radius,
+        surfaceArea(),
+        volume());
+        return infoString;
+    }
 }
--- module4_OOP/src/ShapeArray.java
+++ module4_OOP/src/ShapeArray.java
@@ -2,8 +2,9 @@ public class ShapeArray {
     public static void main(String[] args) {
         // intantiate shapes
         Sphere sphere = new Sphere(5);
+        Cylinder cylinder = new Cylinder(5, 5);
         // add shapes to array
-        Shape[] shapeArray = new Shape[]{sphere};
+        Shape[] shapeArray = new Shape[]{sphere, cylinder};
 
         // loop through array and print shape data
         for (Shape shape: shapeArray) {
