a3cdb1655c5e9d953768ee8dd5ef5af34600b4cd
corwin-bell <corwin.m.bell@gmail.com>
2024-08-19 17:45:56 -0600

    Add private fields and get/set methods for security

--- module6_sorting/src/Student.java
+++ module6_sorting/src/Student.java
@@ -1,12 +1,16 @@
 public class Student {
-    int rollno;  
-    String name;  
-    int age;  
+    private int rollno;  
+    private String name;  
+    private int age;  
     Student(int rollno, String name, int age) {  
         this.rollno=rollno;  
         this.name=name;  
         this.age=age;  
     }
+    public void setRollno(int rollno) { this.rollno = rollno; }
+    public int getRollno() { return rollno; }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
     
     @Override
     public String toString() {
