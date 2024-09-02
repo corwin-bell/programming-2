4394477a800ac37424232e8b67a6f3150e3f235d
corwin-bell <corwin.m.bell@gmail.com>
2024-09-02 12:55:35 -0700

    Fix student object dependent code scope error

--- portfolio_project/src/NameComparator.java
+++ portfolio_project/src/NameComparator.java
@@ -9,15 +9,14 @@ class NameComparator implements Comparator<Student> {
     }
     
     public static void main(String[] args) {
+        NameComparator nc = new NameComparator();
         try {
             Student s1 = new Student(3.50,"Vijay","123 Main St");  
             Student s2 = new Student(4.00,"Ajay","456 Center St");
-            
+            System.out.println("expect compare (Vijay, Ajay) to return positive number: " + nc.compare(s1,s2));
+            System.out.println("expect compare (Ajay, Vijay) to return negative number: " + nc.compare(s2,s1));      
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
-        NameComparator nc = new NameComparator();
-        System.out.println("expect compare (Vijay, Ajay) to return positive number: " + nc.compare(s1,s2));
-        System.out.println("expect compare (Ajay, Vijay) to return negative number: " + nc.compare(s2,s1));
     }
 }
